91e9ef84d809aaf18840792f868b9ad4cba8a05d
Vi4minlove <127950846+Vi4minlove@users.noreply.github.com>
2023-03-20 20:49:38 +0700

    Add files via upload

---
+++ NguyenTrungHieu/NguyenTrungHieu/IStudent.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NguyenTrungHieu
+{
+    interface IStudent
+    {
+        int StudID { set; get; }
+        string StudName { set; get; }
+        string StudGender { set; get; }
+        int StudAge { set; get; }
+        string StudClass { set; get; }
+        float StudAvgMark { get; }
+        void Print();
+    }
+    public class Student : IStudent
+    {
+        public int StudID { set; get; }
+        public string StudName { set; get; }
+        public string StudGender { set; get; }
+        public int StudAge { set; get; }
+        public string StudClass { set; get; }
+        public float StudAvgMark { private set; get; }
+        public void Print()
+        {
+            Console.WriteLine($"StudID: {StudID}\nStudName: {StudName}\nStudGender: {StudGender}\nStudAge: {StudAge}\nStudClass: {StudClass}\nStudAvgMark: {StudAvgMark}");
+            //Console.WriteLine("StudID: " + StudID);
+            //Console.WriteLine("StudName: " + StudName);
+            //Console.WriteLine("StudGender: " + StudGender);
+            //Console.WriteLine("StudAge: " + StudAge);
+            //Console.WriteLine("StudClass: " + StudClass);
+            //Console.WriteLine("StudAvgMark: " + StudAvgMark);
+        }
+        public int[] MarkList = new int[3];
+        public int this[int index]
+        {
+            get { return MarkList[index]; }
+            set { MarkList[index] = value; }
+        }
+        public void CalAvg()
+        {
+            float total = 0;
+            for (int i = 0; i <= MarkList.Length; i++)
+            {
+                total += MarkList[i];
+
+            }
+            StudAvgMark = total / MarkList.Length;
+        }
+        //List<Student> ListStudent = new List<Student>();
+        private List<Student> ListStudent = null;
+        public Student()
+        {
+            ListStudent = new List<Student>();
+        }
+        public void AddStud()
+        {
+            Student st = new Student();
+            Console.Write("StudID: ");
+            st.StudID = System.Int32.Parse(Console.ReadLine());
+            Console.Write("StudName: ");
+            st.StudName = Console.ReadLine();
+            Console.Write("StudGender: ");
+            st.StudGender = Console.ReadLine();
+            Console.Write("StudAge: ");
+            st.StudAge = System.Int32.Parse(Console.ReadLine());
+            Console.Write("StudClass: ");
+            st.StudClass = Console.ReadLine();
+            Console.Write("StudAvgMark: ");
+            for (int i = 0; i < MarkList.Length; i++)
+            {
+                Console.Write($"Mark {i}: ");
+                MarkList[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            ListStudent.Add(st);
+        }
+        public void ShowStud(List<Student> listSV)
+        {
+            if (listSV != null && listSV.Count > 0)
+            {
+                foreach (Student sv in listSV)
+                {
+                    Console.WriteLine($"\nStudID: {sv.StudID}\nStudName: {sv.StudName}\nStudGender: {sv.StudGender}\nStudAge: {sv.StudAge}\nStudClass: {sv.StudClass}\nStudAvgMark: {sv.StudAvgMark}");
+                }
+            }
+            Console.WriteLine();
+        }
+        public List<Student> getListStudent()
+        {
+            return ListStudent;
+        }
+        public Student FindByID(int ID)
+        {
+            Student searchResult = null;
+            if (ListStudent != null && ListStudent.Count > 0)
+            {
+                foreach (Student st in ListStudent)
+                {
+                    if (st.StudID == ID)
+                    {
+                        searchResult = st;
+                    }
+                }
+            }
+            return searchResult;
+        }
+        public List<Student> FindByName(String keyword)
+        {
+            List<Student> searchResult = new List<Student>();
+            if (ListStudent != null && ListStudent.Count > 0)
+            {
+                foreach (Student sv in Student)
+                {
+                    if (sv.StudName.ToUpper().Contains(keyword.ToUpper()))
+                    {
+                        searchResult.Add(sv);
+                    }
+                }
+            }
+            return searchResult;
+        }
+    }
+}
---
+++ NguyenTrungHieu/NguyenTrungHieu/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenTrungHieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student run = new Student();
+            while (true)
+            {
+                Console.WriteLine("       Please select an option:");
+                Console.WriteLine("     =======================================================");
+                Console.WriteLine("       1. Insert new student....");
+                Console.WriteLine("       2. Display all the student list....");
+                Console.WriteLine("       3. Calculator average mark....");
+                Console.WriteLine("       4. Search....");
+                Console.WriteLine("       5. Exit program.");
+                Console.WriteLine("     =======================================================");
+                Console.Write("       Option: ");
+                int key = System.Int32.Parse(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("=>>>Add new Student");
+                        run.AddStud();
+                        Console.WriteLine("DONE!");
+                        break;
+                    case 2:
+                        Console.WriteLine("=>>>Display all Student list");
+                        run.ShowStud(run.getListStudent());
+                        break;
+                    case 3:
+                        Console.WriteLine("=>>>Calculator average markt");
+                        break;
+                    case 4:
+                            Console.WriteLine("               Select an option:");
+                            Console.WriteLine("               =================================");
+                            Console.WriteLine("                1. By ID....");
+                            Console.WriteLine("                2. By Name....");
+                            Console.WriteLine("                3. By Class....");
+                            Console.WriteLine("                4. Back....");
+                            Console.WriteLine("              ==================================");
+                            Console.Write("               Option: ");
+                            int key1 = System.Int32.Parse(Console.ReadLine());
+                            switch (key1)
+                            {
+                                case 1:
+                                    Console.WriteLine(" =>>> Find by ID");
+                                break;
+                                case 2:
+                                    Console.WriteLine(" =>>> Find by Name");
+                                    break;
+                                case 3:
+                                    Console.WriteLine(" =>>> Find by Class");
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Back!");
+                                    break;
+                                default:
+                                    Console.WriteLine("Input is invalid! Please try again: ");
+                                    break;
+                            }                        
+                            break;
+                    case 5:
+                        Console.WriteLine("Exit!");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Input is invalid! Please try again: ");
+                        break;
+                }
+            }
+        }
+    }
+}
